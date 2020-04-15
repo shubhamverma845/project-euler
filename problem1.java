89a4d3048a4b6a664ec1dcb24c96aec700a57f7a
Shubham Verma <shubhamverma845@gmail.com>
2020-04-15 14:16:20 +0530

    problem 1 update

--- problem1.java
+++ problem1.java
@@ -1,20 +1,33 @@
 //multiples of a or b
 
 class problem1{
+
+	static int limit = 1000;
+
+	static int sumDivisibleBy(int n){
+
+		int sum = 0;
+
+		for(int i = n; i < limit; i++){
+			if(i%n == 0){
+				sum += i;
+			}
+		}
+
+		return sum;
+	}
+
 	public static void main(String[] args) {
 		int sum = 0;
 		int a, b;
 		a = 3;
 		b = 5;
 
-		int limit = 10;
-
-		for(int i = 3; i < limit; i++){
-			if(i%3 == 0 || i%5 == 0){
-				sum += i;
-			}
-		}
-
-		System.out.println(sum);
+		// for(int i = 3; i < limit; i++){
+		// 	if(i%3 == 0 || i%5 == 0){
+		// 		sum += i;
+		// 	}
+		// }
+		System.out.println(sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15));
 	}
 }
