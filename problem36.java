2b7fa8ddfc52d487da4185b2067f147cac55e866
Shubham Verma <shubhamverma845@gmail.com>
2020-08-30 21:56:14 +0530

    Problem 36 Added

---
+++ problem36.java
@@ -0,0 +1,49 @@
+//Double-Base Palindromes
+
+class problem36{
+
+	public static boolean toBase2(int num){
+
+		if(num < 2){
+			return true;
+		}
+
+		StringBuilder binRep = new StringBuilder("");
+
+		while(num > 0){
+			binRep.append(Integer.toString(num%2));
+			num = num/2;
+		}
+
+		String s1 = binRep.toString();
+		binRep.reverse();
+		String s2 = binRep.toString();
+
+		return s1.equals(s2);
+
+	}
+
+	public static boolean isPalindrome(String num){
+		StringBuilder numRev = new StringBuilder(num);
+		numRev.reverse();
+
+		if(num.equals(numRev.toString())){
+			return true;
+		}
+
+		return false;
+	}
+	
+	public static void main(String[] args) {
+
+		int sum = 0;
+
+		for(int i = 0; i < 1000000; i++){
+			if(isPalindrome(Integer.toString(i)) && toBase2(i)){
+				sum += i;
+			}
+		}
+
+		System.out.println(sum);
+	}
+}
